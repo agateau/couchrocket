a14e94207a082582f686d53053ff5a605d233272
Aurélien Gâteau <mail@agateau.com>
2016-09-18 18:25:06 +0200

    Add quick'n'dirty fallback image

--- iconpixmapprovider.cpp
+++ iconpixmapprovider.cpp
@@ -1,7 +1,19 @@
 #include "iconpixmapprovider.h"
 
 #include <QDebug>
+#include <QFile>
 #include <QIcon>
+#include <QPainter>
+
+static QPixmap scalePix(const QPixmap &fg_, const QSize &size)
+{
+    QPixmap fg = fg_.scaled(size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    QPixmap pix(size);
+    pix.fill(Qt::transparent);
+    QPainter painter(&pix);
+    painter.drawPixmap(size.width() - fg.width(), size.height() - fg.height(), fg);
+    return pix;
+}
 
 IconPixmapProvider::IconPixmapProvider()
     : QQuickImageProvider(QQuickImageProvider::Pixmap)
@@ -11,13 +23,22 @@ IconPixmapProvider::IconPixmapProvider()
 
 QPixmap IconPixmapProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    QIcon icon = QIcon::fromTheme(id);
-    if (icon.isNull()) {
-        icon = QIcon::fromTheme("applications-system");
+    QPixmap pix;
+    if (QFile::exists(id)) {
+        pix.load(id);
+    } else {
+        QIcon icon = QIcon::fromTheme(id);
+        if (icon.isNull()) {
+            icon = QIcon::fromTheme("applications-system");
+        }
+        pix = icon.pixmap(requestedSize);
     }
-    QPixmap pix = icon.pixmap(requestedSize);
+    if (pix.isNull()) {
+        pix.load(":/images/rocket.svg");
+    }
+
     if (pix.size() != requestedSize) {
-        pix = pix.scaled(requestedSize);
+        pix = scalePix(pix, requestedSize);
     }
     if (size) {
         *size = pix.size();
