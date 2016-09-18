6dff6eb5d5fbb0c89a72b52bd58c749985aaaef1
Aurélien Gâteau <mail@agateau.com>
2016-09-18 17:19:25 +0200

    Fix stray space

--- launcher.cpp
+++ launcher.cpp
@@ -8,7 +8,7 @@ Launcher::Launcher(const QString &desktopFilePath)
     QSettings settings(desktopFilePath, QSettings::IniFormat);
     settings.beginGroup("Desktop Entry");
     mName = settings.value("Name").toString();
-    mIconUrl = QUrl("image://icon/" +  settings.value("Icon").toString());
+    mIconUrl = QUrl("image://icon/" + settings.value("Icon").toString());
     mExec = settings.value("Exec").toString();
     mExec.replace("%u", "");
     mExec.replace("%U", "");
