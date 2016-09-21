cf2c505629d75fef96a90e43cc0090a61e92cfa6
Aurélien Gâteau <mail@agateau.com>
2016-09-21 08:24:26 +0200

    Do not use QSettings to read .desktop files
    If a value contains a comma, QSettings interprets it as a list. The value
    must be inside quotes to be interpreted as a string. Parse the .desktop
    file ourselves to avoid this bug.

--- src/core/launcher.cpp
+++ src/core/launcher.cpp
@@ -1,16 +1,60 @@
 #include "launcher.h"
 
 #include <QDebug>
+#include <QFile>
 #include <QProcess>
-#include <QSettings>
+#include <QTextStream>
+
+static constexpr char GROUP_START[] = "[Desktop Entry]";
+
+static QHash<QString, QString> readDesktopFile(QIODevice *device)
+{
+    QTextStream stream(device);
+    QHash<QString, QString> hash;
+    bool reachedStart = false;
+    while (!stream.atEnd()) {
+        QString line = stream.readLine().trimmed();
+        // Skip empty lines and comments
+        if (line.isEmpty() || line[0] == '#') {
+            continue;
+        }
+        if (reachedStart) {
+            int idx = line.indexOf('=');
+            if (idx == -1) {
+                qWarning() << "Invalid line" << line;
+                continue;
+            }
+            QString key = line.left(idx).trimmed();
+            QString value = line.mid(idx + 1).trimmed();
+            hash[key] = value;
+        } else {
+            if (line == GROUP_START) {
+                reachedStart = true;
+            }
+        }
+    }
+    return hash;
+}
 
 Launcher::Launcher(const QString &desktopFilePath)
 {
-    QSettings settings(desktopFilePath, QSettings::IniFormat);
-    settings.beginGroup("Desktop Entry");
-    mName = settings.value("Name").toString();
-    mIconUrl = QUrl("image://icon/" + settings.value("Icon").toString());
-    mExec = settings.value("Exec").toString();
+    QHash<QString, QString> hash;
+    {
+        QFile file(desktopFilePath);
+        if (!file.open(QIODevice::ReadOnly)) {
+            qWarning() << "Cannot read" << desktopFilePath;
+            return;
+        }
+        hash = readDesktopFile(&file);
+        if (hash.isEmpty()) {
+            qWarning() << desktopFilePath << "contains no .desktop entries";
+            return;
+        }
+    }
+
+    mName = hash.value("Name");
+    mIconUrl = QUrl("image://icon/" + hash.value("Icon"));
+    mExec = hash.value("Exec");
     mExec.replace("%u", "");
     mExec.replace("%U", "");
     mExec.replace("%f", "");
