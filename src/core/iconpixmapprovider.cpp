4f4e2379eca06a13f13f02bd8f23b6f68c47fdf7
Aurélien Gâteau <mail@agateau.com>
2018-12-31 00:33:13 +0100

    Add image caching and default image

--- src/core/iconpixmapprovider.cpp
+++ src/core/iconpixmapprovider.cpp
@@ -6,7 +6,8 @@
 #include <QImageReader>
 #include <QPainter>
 
-static constexpr char UNKNOWN_IMAGE[] = ":/images/broken.svg";
+static constexpr char BROKEN_IMAGE[] = ":/images/broken.svg";
+static constexpr char DEFAULT_IMAGE[] = ":/images/rocket.svg";
 
 static constexpr char FALLBACK_DIR[] = "/usr/share/pixmaps";
 static constexpr const char *FALLBACK_EXTS[] = {".svg", ".png", ".xpm", 0};
@@ -27,23 +28,32 @@ static QPixmap loadPixmapFromPath(const QString &path, const QSize &requestedSiz
         return QPixmap();
     }
     QImageReader reader(path);
-    QSize size = reader.size().scaled(requestedSize, Qt::KeepAspectRatioByExpanding);
-    reader.setScaledSize(size);
+    if (!requestedSize.isEmpty()) {
+        QSize size = reader.size().scaled(requestedSize, Qt::KeepAspectRatioByExpanding);
+        reader.setScaledSize(size);
+    }
     return QPixmap::fromImage(reader.read());
 }
 
 static QPixmap loadRawPixmap(const QString &id, const QSize &requestedSize)
 {
+    if (id.isEmpty()) {
+        return loadPixmapFromPath(DEFAULT_IMAGE, requestedSize);
+    }
+
+    // Try to consider id as a full path
     QPixmap pix = loadPixmapFromPath(id, requestedSize);
     if (!pix.isNull()) {
         return pix;
     }
 
+    // No luck, is it a theme icon?
     QIcon icon = QIcon::fromTheme(id);
     if (!icon.isNull()) {
-        return icon.pixmap(requestedSize);
+        return icon.pixmap(qMax(requestedSize.width(), requestedSize.height()));
     }
 
+    // Still no luck, look in the fallback dir
     QString dir = QString(FALLBACK_DIR) + '/';
     for (auto ext = FALLBACK_EXTS; *ext != 0; ++ext) {
         QString path = dir + id + *ext;
@@ -53,7 +63,8 @@ static QPixmap loadRawPixmap(const QString &id, const QSize &requestedSize)
         }
     }
 
-    return loadPixmapFromPath(UNKNOWN_IMAGE, requestedSize);
+    // This icon is broken
+    return loadPixmapFromPath(BROKEN_IMAGE, requestedSize);
 }
 
 IconPixmapProvider::IconPixmapProvider()
@@ -64,9 +75,24 @@ IconPixmapProvider::IconPixmapProvider()
 
 QPixmap IconPixmapProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    QPixmap pix = loadRawPixmap(id, requestedSize);
+    int extent = qMax(requestedSize.width(), requestedSize.height());
+    const QString cacheId = QString("%1-%2").arg(id).arg(extent);
 
-    if (pix.size() != requestedSize) {
+    QPixmap pix;
+    {
+        QMutexLocker lock(&mMutex);
+        auto it = mCache.find(cacheId);
+        if (it != mCache.end()) {
+            pix = *it;
+        }
+    }
+    if (pix.isNull()) {
+        pix = loadRawPixmap(id, {extent, extent});
+        QMutexLocker lock(&mMutex);
+        mCache.insert(cacheId, pix);
+    }
+
+    if (!requestedSize.isEmpty() && pix.size() != requestedSize) {
         pix = scalePixmap(pix, requestedSize);
     }
     if (size) {
--- src/core/iconpixmapprovider.h
+++ src/core/iconpixmapprovider.h
@@ -3,12 +3,19 @@
 
 #include <QQuickImageProvider>
 
+#include <QMutex>
+#include <QHash>
+
 class IconPixmapProvider : public QQuickImageProvider
 {
 public:
     IconPixmapProvider();
 
     QPixmap requestPixmap(const QString &id, QSize *size, const QSize &requestedSize) override;
+
+private:
+    QMutex mMutex;
+    QHash<QString, QPixmap> mCache;
 };
 
 #endif // ICONPIXMAPPROVIDER_H
